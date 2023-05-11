9736fd9cc5337b92a58234b44e6df1219c19e9e0
Caroline Stadnicki <carolinestadnicki.nz@gmail.com>
2023-05-11 16:12:58 +1200

    Update projects description

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -19,7 +19,7 @@ const Work = () => {
         </div>
 
 {/* container for projects */}
-<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+<div className="grid sm:grid-cols-2 md:grid-cols-4 gap-4">
           
           {/* Grid Item */}
           {project.map((item, index) => {
--- src/data/data.js
+++ src/data/data.js
@@ -9,7 +9,7 @@ export const data=[
         image: SweetAsOrganics,
         github:"https://github.com/harakeke-2023/sweet-as-organics-api-rtk/tree/caroline",
         live:"",
-        description: "To complete this project I had to manage relational data in a database and create a RESTful API using Node.js, Express and PostgreSQL. I also had to create a React application that consumes the API and uses Redux for state management.",
+        description: "To complete 'Sweet As Organics' I had to manage relational data in a database and create a RESTful API using Node.js, Express and PostgreSQL. I also had to create a React application that consumes the API and uses Redux for state management.",
     },
     {
         id:2,
@@ -17,7 +17,7 @@ export const data=[
         image:Waitless,
         github:"https://github.com/harakeke-2023/Waitless",
         live:"",
-        description:"In this group project I created the menu page for the user and the admin views as well as the tests for those components, while collaborating with 4 other students to create a full stack application. We used React, Redux, Node.js, Express, PostgreSQL and TailwindCSS."
+        description:"In 'Waitless', I created the menu page for the user and the admin views as well as the tests for those components, while collaborating with 4 other students to create a full stack application. We used React, Redux, Node.js, Express, PostgreSQL and TailwindCSS."
     },
     {
         id:3,
@@ -25,7 +25,7 @@ export const data=[
         image:Dreamfest,
         github:"https://github.com/harakeke-2023/dreamfest/tree/caroline",
         live:"",
-        description:"That was a fun exercise that I completed during Bootcamp to practice managing relational databases and testing.",
+        description:"'Dreamfest' was a fun project that I completed during Bootcamp to practice managing relational databases and testing.",
     },
     // {
     //     id:4,
